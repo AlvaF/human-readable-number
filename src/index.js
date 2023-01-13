7bd6b4944f034f051b4eb20f2ac429564676cfe9
Alvina <Es_96@mail.ru>
2023-01-13 20:12:21 +0700

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -8,22 +8,33 @@ module.exports = function toReadable(num) {
   let str = String(num);
   if (str.length == 1) {
     answer += n[num - 1]
+    if (str[0] == 0) {
+      answer = 'zero'
+    }
   } else if (str.length == 2) {
     if (str[0] == 1) {
-      answer = m[str[1] - 2]
-    } else {
+      answer = m[str[1]]
+    } else if (str[1] == 0) {
+      answer = u[str[0] - 1]
+    }
+    else {
       answer = u[str[0] - 1] + ' ' + n[str[1] - 1]
     }
   } else if (str.length == 3) {
-    if (str[1] != 0) {
+    if (str[1] == 0 && str[2] == 0) {
+      answer = n[str[0] - 1] + ' ' + b[0]
+    }
+
+    else if (str[1] != 0) {
       answer = n[str[0] - 1] + ' ' + b[0] + ' ' + u[str[1] - 1] + ' ' + n[str[2] - 1]
-    } else {
+    }
+    else if (str[1] == 0) {
       answer = n[str[0] - 1] + ' ' + b[0] + ' ' + n[str[2] - 1]
     }
     if (str[1] == 1) {
       answer = n[str[0] - 1] + ' ' + b[0] + ' ' + m[str[2]]
     }
-    else if (str[2] == 0) {
+    else if (str[2] == 0 && str[1] != 0) {
       answer = n[str[0] - 1] + ' ' + b[0] + ' ' + u[str[1] - 1]
     }
 
